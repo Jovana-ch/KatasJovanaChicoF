e36a9d5ac700aa4400cf6a1f139eeaa509f13bf7
Jovana-ch <72950489+Jovana-ch@users.noreply.github.com>
2022-02-22 00:27:59 -0600

    add: files from cloud gui

---
+++ prueba.py
@@ -0,0 +1,8 @@
+from datetime import date 
+date.today()
+print(date.today())
+print("Today's date is: " + str(date.today()))
+print("Hola desde una consola")
+sum=1+2
+product = sum *2
+print(product)
